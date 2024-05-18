4a8f787da22178735b208b1b8719b8ddcfeb184b
Bharad Raghavan <46429739+bharadr@users.noreply.github.com>
2024-05-18 16:09:22 -0400

    Got the design just started. Got personal info mostly done

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,15 +1,28 @@
 import '../App.css'
 
+function PersonalInfoSection({fullName, address, email, phoneNumber, summary}) {
+    return (
+        <div id="display-personal-info">
+            <div id="personal-contact">
+                <div>{address}</div>
+                <div>
+                    <div>{email}</div>
+                    <div>{phoneNumber}</div>
+                </div>
+            </div>
+            <div id="personal-name">{fullName}</div>
+            <div id="personal-description">{summary}</div>
+        </div>
+    )
+}
+
+
+
+
 function Display({personalInfo, skillEntries, educationEntries, workplaceEntries}) {
   return (
     <div id="displaySection">
-        <div id="display-personal-info">
-            {personalInfo.fullName}
-            {personalInfo.address}
-            {personalInfo.email}
-            {personalInfo.phoneNumber}
-            {personalInfo.summary}
-        </div>
+        <PersonalInfoSection fullName={personalInfo.fullName} address={personalInfo.address} email={personalInfo.email} phoneNumber={personalInfo.phoneNumber} summary={personalInfo.summary}></PersonalInfoSection>
         <div id="display-education-info">
             {educationEntries.map((entry, index) => (
                 <div key={index}>{entry.contents.schoolName} {entry.contents.startDate} {entry.contents.endDate} {entry.contents.degreeMajor} {entry.contents.educationLocation}</div>
@@ -17,7 +30,7 @@ function Display({personalInfo, skillEntries, educationEntries, workplaceEntries
         </div>
         <div id="display-workplace-info">
             {workplaceEntries.map((entry, index) => (
-                <div key={index}>{entry.contents.companyName} {entry.contents.companyTitle} {entry.contents.endDate} {entry.contents.startDate} {entry.contents.educationLocation}</div>
+                <div key={index}>{entry.contents.companyName} {entry.contents.companyTitle} {entry.contents.endDate} {entry.contents.startDate} {entry.contents.companyLocation} {entry.contents.companyDescription}</div>
             ))}
         </div>
         <div id="display-skills-info">
