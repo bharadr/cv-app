3241d6a2ff3a189468a32b388ee8d230d5f9ee2a
Bharad Raghavan <46429739+bharadr@users.noreply.github.com>
2024-05-20 18:45:06 -0400

    Got it working with pre-filled stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 import './App.css'
@@ -16,9 +16,6 @@ const defaultPersonalInfo = {
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState(defaultPersonalInfo);
-  const [skillEntries, setSkillEntries] = useState([]);
-  const [educationEntries, setEducationEntries] = useState([]);
-  const [workplaceEntries, setWorkplaceEntries] = useState([]);
 
   // Handler function to update the state
   const handlePersonalInfoChange = (field, value) => {
@@ -32,6 +29,58 @@ function App() {
     setWorkplaceEntries((prevEntries) => prevEntries.filter(entry => entry.id !== id));
   }
 
+  const defaultWorkplaceEntries = [
+    {
+      key: 0,
+      id: 0,
+      contents: {
+        'companyName': 'Bobble',
+        'companyTitle': 'Senior Software Engineer - Infrastructure',
+        'companyLocation': 'Burlingame, CA',
+        'startDate': '01/2014',
+        'endDate': '10/2017',
+        'companyDescription': `Co-led the Bobble backend team, with 200M+ users and 1M+ DAU.
+        Used Node.js, Postgres, Redis, Elasticsearch and Cassandra.
+        Implemented sharing posts, adding topics to posts, likes, mentions in an agile team.
+        Used Elasticsearch to query hundreds of millions of documents accurately and return results in under a second.`
+      },
+      removeFn: () => removeWorkplaceEntry(0),
+    },
+    {
+      key: 1,
+      id: 1,
+      contents: {
+        'companyName': 'Bobble',
+        'companyTitle': 'Senior Software Engineer - Infrastructure',
+        'companyLocation': 'Burlingame, CA',
+        'startDate': '01/2014',
+        'endDate': '10/2017',
+        'companyDescription': `Co-led the Bobble backend team, with 200M+ users and 1M+ DAU.
+        Used Node.js, Postgres, Redis, Elasticsearch and Cassandra.
+        Implemented sharing posts, adding topics to posts, likes, mentions in an agile team.
+        Used Elasticsearch to query hundreds of millions of documents accurately and return results in under a second.`
+      },
+      removeFn: () => removeWorkplaceEntry(1),
+    },
+    {
+      key: 2,
+      id: 2,
+      contents: {
+        'companyName': 'Bobble',
+        'companyTitle': 'Senior Software Engineer - Infrastructure',
+        'companyLocation': 'Burlingame, CA',
+        'startDate': '01/2014',
+        'endDate': '10/2017',
+        'companyDescription': `Co-led the Bobble backend team, with 200M+ users and 1M+ DAU.
+        Used Node.js, Postgres, Redis, Elasticsearch and Cassandra.
+        Implemented sharing posts, adding topics to posts, likes, mentions in an agile team.
+        Used Elasticsearch to query hundreds of millions of documents accurately and return results in under a second.`
+      },
+      removeFn: () => removeWorkplaceEntry(2),
+    },
+  ]
+  const [workplaceEntries, setWorkplaceEntries] = useState(defaultWorkplaceEntries);
+
   const addWorkplaceEntry = () => {
     let id = Date.now();
     const newEntry = {
@@ -43,9 +92,70 @@ function App() {
     setWorkplaceEntries([...workplaceEntries, newEntry]);
   }
 
+  const handleWorkplaceChange = (id, field, value) => {
+    const index = workplaceEntries.findIndex((entry) => entry.id === id);
+    if (index !== -1) {
+      let desiredEntry = workplaceEntries[index];
+      const updatedEntry = {
+        ...desiredEntry,
+        contents: {
+          ...desiredEntry.contents,
+          [field]: value,
+        },
+      };
+      const updatedEntries = [
+        ...workplaceEntries.slice(0, index),
+        updatedEntry,
+        ...workplaceEntries.slice(index + 1),
+      ];
+      setWorkplaceEntries(updatedEntries)
+    }
+  }
+
   const removeEducationEntry = (id) => {
     setEducationEntries((prevEntries) => prevEntries.filter(entry => entry.id !== id));
   }
+
+  const defaultEducationEntries = [
+    {
+      key: 0,
+      id: 0,
+      contents: {
+        'schoolName': 'Stanford University',
+        'degreeMajor': 'B.S. Computer Science',
+        'educationLocation': 'Palo Alto, CA',
+        'startDate': '01/2014',
+        'endDate': '10/2017',
+      },
+      removeFn: () => removeEducationEntry(0),
+    },
+    {
+      key: 1,
+      id: 1,
+      contents: {
+        'schoolName': 'Stanford University',
+        'degreeMajor': 'B.S. Computer Science',
+        'educationLocation': 'Palo Alto, CA',
+        'startDate': '01/2014',
+        'endDate': '10/2017',
+      },
+      removeFn: () => removeEducationEntry(1),
+    },
+    {
+      key: 2,
+      id: 2,
+      contents: {
+        'schoolName': 'Stanford University',
+        'degreeMajor': 'B.S. Computer Science',
+        'educationLocation': 'Palo Alto, CA',
+        'startDate': '01/2014',
+        'endDate': '10/2017',
+      },
+      removeFn: () => removeEducationEntry(2),
+    },
+  ]
+
+  const [educationEntries, setEducationEntries] = useState(defaultEducationEntries);
 
   const addEducationEntry = () => {
     let id = Date.now();
@@ -57,10 +167,58 @@ function App() {
     };
     setEducationEntries([...educationEntries, newEntry]);
   }
+  
+  const handleEducationalChange = (id, field, value) => {
+    const index = educationEntries.findIndex((entry) => entry.id === id);
+    if (index !== -1) {
+      let desiredEntry = educationEntries[index];
+      const updatedEntry = {
+        ...desiredEntry,
+        contents: {
+          ...desiredEntry.contents,
+          [field]: value,
+        },
+      };
+      const updatedEntries = [
+        ...educationEntries.slice(0, index),
+        updatedEntry,
+        ...educationEntries.slice(index + 1),
+      ];
+      setEducationEntries(updatedEntries)
+    }
+  }
 
   const removeSkillEntry = (id) => {
     setSkillEntries((prevEntries) => prevEntries.filter(entry => entry.id !== id));
   }
+
+  const defaultSkillEntries = [
+    {
+      key: 0,
+      id: 0,
+      contents: {
+        'skillName': 'C++'
+      },
+      removeFn: () => removeSkillEntry(0),
+    },
+    {
+      key: 1,
+      id: 1,
+      contents: {
+        'skillName': 'Writing'
+      },
+      removeFn: () => removeSkillEntry(1),
+    },
+    {
+      key: 2,
+      id: 2,
+      contents: {
+        'skillName': 'Tableau'
+      },
+      removeFn: () => removeSkillEntry(2),
+    },
+  ]
+  const [skillEntries, setSkillEntries] = useState(defaultSkillEntries);
 
   const addSkillEntry = () => {
     let id = Date.now();
@@ -93,49 +251,6 @@ function App() {
       setSkillEntries(updatedEntries)
     }
   }
-
-  const handleEducationalChange = (id, field, value) => {
-    const index = educationEntries.findIndex((entry) => entry.id === id);
-    if (index !== -1) {
-      let desiredEntry = educationEntries[index];
-      const updatedEntry = {
-        ...desiredEntry,
-        contents: {
-          ...desiredEntry.contents,
-          [field]: value,
-        },
-      };
-      const updatedEntries = [
-        ...educationEntries.slice(0, index),
-        updatedEntry,
-        ...educationEntries.slice(index + 1),
-      ];
-      setEducationEntries(updatedEntries)
-    }
-  }
-
-  const handleWorkplaceChange = (id, field, value) => {
-    const index = workplaceEntries.findIndex((entry) => entry.id === id);
-    if (index !== -1) {
-      let desiredEntry = workplaceEntries[index];
-      const updatedEntry = {
-        ...desiredEntry,
-        contents: {
-          ...desiredEntry.contents,
-          [field]: value,
-        },
-      };
-      const updatedEntries = [
-        ...workplaceEntries.slice(0, index),
-        updatedEntry,
-        ...workplaceEntries.slice(index + 1),
-      ];
-      setWorkplaceEntries(updatedEntries)
-    }
-  }
-
-
-
 
   return (
     <div id="overallLayout">
--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -3,64 +3,54 @@ import '../styles/forms.css'
 
 
 
-function EducationEntry({id, removeEntry, handleChange}) {
+function EducationEntry({id, removeEntry, handleChange, contents}) {
     return (
         <div className="education-entry">
             <div>
-                <div>
-                    <label htmlFor="schoolName">School Name:</label>
-                    <input type="text" id={`schoolName-${id}`} name="schoolName" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="degreeMajor">Degree and Major:</label>
-                    <input type="text" id={`degreeMajor-${id}`} name="degreeMajor" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="educationLocation">Location:</label>
-                    <input type="text" id={`educationLocation-${id}`} name="educationLocation" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="startDate">Start Date:</label>
-                    <input type="text" id={`education-startDate-${id}`} name="startDate" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
-                </div>
-                <div>
-                    <label htmlFor="endDate">End Date:</label>
-                    <input type="text" id={`education-endDate-${id}`} name="endDate" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
-                </div>
+                <label htmlFor="schoolName">School Name:</label>
+                <input type="text" id={`schoolName-${id}`} name="schoolName"  value={contents.schoolName} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <label htmlFor="degreeMajor">Degree and Major:</label>
+                <input type="text" id={`degreeMajor-${id}`} name="degreeMajor" value={contents.degreeMajor} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <label htmlFor="educationLocation">Location:</label>
+                <input type="text" id={`educationLocation-${id}`} name="educationLocation" value={contents.educationLocation} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <label htmlFor="startDate">Start Date:</label>
+                <input type="text" id={`education-startDate-${id}`} name="startDate" value={contents.startDate} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <label htmlFor="endDate">End Date:</label>
+                <input type="text" id={`education-endDate-${id}`} name="endDate" value={contents.endDate} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
             </div>
             <button className="entry-delete-button" onClick={() => removeEntry(id)}>Remove</button>
         </div>
     )
 }
 
-function SkillEntry({id, removeEntry, handleChange}) {
+function SkillEntry({id, removeEntry, handleChange, skillName}) {
     return (
         <div className="skill-entry">
             <div>
                 <label htmlFor="skill-name">Skill Name:</label>
-                <input type="text" id={`skill-name-${id}`}  name="skill-name" onChange={(e) => handleChange(id, e.target.value)}/>
+                <input type="text" id={`skill-name-${id}`}  name="skill-name" value={skillName} onChange={(e) => handleChange(id, e.target.value)}/>
             </div>
             <button className="entry-delete-button" onClick={() => removeEntry(id)}>Remove</button>
         </div>
     )
 }
 
-function WorkplaceEntry({id, removeEntry, handleChange}) {
+function WorkplaceEntry({id, removeEntry, handleChange, contents}) {
     return (
         <div className="workplace-entry">
             <div>
                 <label htmlFor="companyName">Company Name:</label>
-                <input type="text" id={`companyName-${id}`} name="companyName" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <input type="text" id={`companyName-${id}`} name="companyName" value={contents.companyName} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
                 <label htmlFor="companyTitle">Title:</label>
-                <input type="text" id={`companyTitle-${id}`} name="companyTitle" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <input type="text" id={`companyTitle-${id}`} name="companyTitle" value={contents.companyTitle} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
                 <label htmlFor="companyLocation">Location:</label>
-                <input type="text" id={`companyLocation-${id}`} name="companyLocation" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <input type="text" id={`companyLocation-${id}`} name="companyLocation" value={contents.companyLocation} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
                 <label htmlFor="startDate">Start Date:</label>
-                <input type="text" id={`company-startDate-${id}`} name="startDate" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <input type="text" id={`company-startDate-${id}`} name="startDate" value={contents.startDate} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
                 <label htmlFor="endDate">End Date:</label>
-                <input type="text" id={`company-endDate-${id}`} name="endDate" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <input type="text" id={`company-endDate-${id}`} name="endDate" value={contents.endDate} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
                 <label htmlFor="companyDescription">Description:</label>
-                <textarea id={`companyDescription-${id}`} name="companyDescription" onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
+                <textarea id={`companyDescription-${id}`} name="companyDescription" value={contents.companyDescription} onChange={(e) => handleChange(id, e.target.name, e.target.value)}/>
             </div>
             <button className="entry-delete-button" onClick={() => removeEntry(id)}>Remove</button>
         </div>
@@ -85,11 +75,11 @@ function SectionInfo({name, entries, addEntry, handleChange}) {
       <div className="form-expanded-section" style={{ display: isExpanded ? 'flex' : 'none'}}>
         {entries.map(entry => {
             if (name === "Educational Background") {
-                return <EducationEntry key={entry.key} id={entry.id} removeEntry={entry.removeFn} handleChange={handleChange}/>
+                return <EducationEntry key={entry.key} id={entry.id} removeEntry={entry.removeFn} handleChange={handleChange} contents={entry.contents}/>
             } else if (name === "Professional Experiences") {
-                return <WorkplaceEntry key={entry.key} id={entry.id} removeEntry={entry.removeFn} handleChange={handleChange}/>
+                return <WorkplaceEntry key={entry.key} id={entry.id} removeEntry={entry.removeFn} handleChange={handleChange} contents={entry.contents}/>
             } else {
-                return <SkillEntry key={entry.key} id={entry.id} removeEntry={entry.removeFn} handleChange={handleChange}/>
+                return <SkillEntry key={entry.key} id={entry.id} removeEntry={entry.removeFn} handleChange={handleChange} skillName={entry.contents.skillName}/>
             }
             })
         }
